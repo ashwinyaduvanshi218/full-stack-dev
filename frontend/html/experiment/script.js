941ee362dddb4278d15c400c190f566e1fc15f99
Ashwin kumar yadav <ashwinyaduvanshi218@gmail.com>
2025-11-18 09:40:30 +0530

    Create script.js

---
+++ frontend/html/experiment/script.js
@@ -0,0 +1,15 @@
+/*console.log("welcome")
+
+console.log("helloe hello");
+const username= prompt("what is your name  ");
+alert("hello  "+ username + " nice to meet you");
+
+let age = prompt("Enter your age:");
+
+let result = (age >= 18) ? alert("You can vote!") : alert("You cannot vote!");
+console.log(result);*/
+
+const age= prompt("what is your age ");
+ age >=18? document.write("<h1>you are an adult. </h1>"):document.write("<h1>you are a minaor.</h1>");
+
+console.log(age);
